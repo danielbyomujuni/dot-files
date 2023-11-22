a7cd6c94059b5e83ce5e3a56adba4b8c0d5de39b
Daniel <danielbyomujuni@gmail.com>
2023-11-22 16:56:05 +0000

    Added Neofetch and Updated PC Status

--- eww/scripts/currentwindow.sh
+++ eww/scripts/currentwindow.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-hyprctl activewindow -j | jq -r '.initialTitle'
+window=$(hyprctl activewindow -j | jq -r '.initialTitle')
+
+if [ $window = null ]; then
+  echo "Desktop";
+else
+  echo $window;
+fi;
