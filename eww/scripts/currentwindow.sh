09b3700b2fc7212eb8bdf7630adc42615747bc96
Daniel <danielbyomujuni@gmail.com>
2023-12-02 23:14:24 +0000

    Miror Tweaks & Lethal Comanpy Gaming Improvements

--- eww/scripts/currentwindow.sh
+++ eww/scripts/currentwindow.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 window=$(hyprctl activewindow -j | jq -r '.initialTitle')
 
+
+
 if [ $window = null ]; then
   echo "Desktop";
-else
-  echo $window;
+  exit
 fi;
+
+if [[ $window =~ ^OBS.* ]]; then
+  echo "OBS Studio";
+  exit
+fi;  
+
+echo $window;
+exit
+
